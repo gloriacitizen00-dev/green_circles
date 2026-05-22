e10b2a7f21d4782766f884c5967043f2035e155e
Gloria NeuroCinematic <gloriacitizen.00@gmail.com>
2026-05-22 08:26:42 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,9 @@
-let streams = [];
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont('monospace');
-  textSize(20);
-
-  // columnas Matrix
-  let x = 0;
-  for (let i = 0; i < width / 20; i++) {
-    streams.push(new Stream(x, random(-1000, 0)));
-    x += 20;
-  }
 }
 
 function draw() {
   background(0);
-  fill(255,0,0);
-  circle(100,100,50);
-}
-
-class Stream {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.speed = random(4, 12);
-    this.symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  }
-
-  render() {
-    let char = this.symbols.charAt(floor(random(this.symbols.length)));
-    fill(0, 255, 70);
-    text(char, this.x, this.y);
-    this.y += this.speed;
-
-    if (this.y > height) {
-      this.y = random(-200, 0);
-    }
-  }
+  fill(255, 0, 0);
+  circle(width/2, height/2, 100);
 }
