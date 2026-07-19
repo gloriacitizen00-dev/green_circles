1bc9a3f97e26295438dd1db1d1619c929a48c1f6
SkyCortex <gloriacitizen.00@gmail.com>
2026-07-18 21:29:20 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,37 +6,30 @@ function preload() {
 }
 
 function setup() {
- for (let i = 0; i < tabla.getRowCount(); i++) {
-  sorteo.push(random(50, width - 50));
-}
+  createCanvas(windowWidth, 500);
   textFont("monospace");
 
   // posiciones aleatorias para tus datos
-for (let i = 0; i < tabla.getRowCount(); i++) {
-  sorteo.push(random(50, width - 50));
-}
-}
+for (let i = 0; i < 100; i++) { sorteo.push(random(50, 750)); } }
 
 function draw() {
-  background(255, 255, 255);
+ background(255, 255, 255);
 
   for (let i = 0; i < tabla.getRowCount(); i++) {
-    let value = int(tabla.getString(i, 1));
+   let value = int(tabla.getString(i, 1));
 
-    let x = sorteo[i] + sin(frameCount * 0.05 + i) * 20;
-    let y = random(50,450); + cos(frameCount * 0.03 + i) * 20;
-    let r = map(value, 50, 230, 20, 70);
+   let x = sorteo[i] + sin(frameCount * 0.05 + i) * 20; 
+   let y = map(value, 50, 259, 0, 800) + cos(frameCount * 0.03 + i) * 20; 
+   let r = map(value, 50, 230, 30, 100);
 
     // círculos verdes
     fill(0, 255, 70, 120);
     noStroke();
     circle(x, y, r);
 
-    
-    // texto blanco de las palabras
+
+    // palabras dentro de los círculos
     fill(255, 220, 0);
-    textSize(map(value, 50, 259, 12, 28));
-    textAlign(CENTER, CENTER);
     text(tabla.getString(i, 0), x, y);
   }
 }
