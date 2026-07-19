353f20ab9deaa2444406a825fdfc2d2dabd7fc48
SkyCortex <gloriacitizen.00@gmail.com>
2026-07-18 21:23:20 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,15 +6,15 @@ function preload() {
 }
 
 function setup() {
- for (let i = 0; i < 100; i++) {
+ for (let i = 0; i < tabla.getRowCount(); i++) {
   sorteo.push(random(50, width - 50));
 }
   textFont("monospace");
 
   // posiciones aleatorias para tus datos
-  for (let i = 0; i < 100; i++) {
-    sorteo.push(random(50, 750));
-  }
+for (let i = 0; i < tabla.getRowCount(); i++) {
+  sorteo.push(random(50, width - 50));
+}
 }
 
 function draw() {
@@ -24,7 +24,7 @@ function draw() {
     let value = int(tabla.getString(i, 1));
 
     let x = sorteo[i] + sin(frameCount * 0.05 + i) * 20;
-    let y = map(value, 50, 259, 50, 450) + cos(frameCount * 0.03 + i) * 20;
+    let y = random(50,450); + cos(frameCount * 0.03 + i) * 20;
     let r = map(value, 50, 230, 20, 70);
 
     // círculos verdes
